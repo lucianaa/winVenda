0941e34f909a92d6dfc8cf10a0daed56e6837fa0
Luciana Balieiro <luciana.balieiro@gmail.com>
2011-07-03 14:48:41 -0300

    Alterando os dados do Assembly Info

--- winVenda/Properties/AssemblyInfo.cs
+++ winVenda/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("winVenda")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("IFNMG")]
 [assembly: AssemblyProduct("winVenda")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © IFNMG 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
